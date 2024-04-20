4d346e973dee59b0bc8ae93f4de5830a6d63713b
tanyakanf <tanyakanf@gmail.com>
2024-04-20 18:29:12 +0200

    done

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,4 +1,5 @@
 import placeholderImage from "../assets/profile-icon.png";
+import { Link } from "react-router-dom";
 
 function StudentCard({
   _id,
@@ -11,6 +12,7 @@ function StudentCard({
   className,
 }) {
   return (
+    <Link to={`/students/${_id}`}>
       <div
         className={`StudentCard flex justify-between items-center p-3 mb-2 bg-white shadow-sm rounded border border-gray-200 hover:bg-gray-50 ${className}`}
       >
@@ -35,6 +37,7 @@ function StudentCard({
         <span style={{ flexBasis: "20%" }}>{email}</span>
         <span style={{ flexBasis: "20%" }}>{phone}</span>
       </div>
+    </Link>
   );
 }
 
